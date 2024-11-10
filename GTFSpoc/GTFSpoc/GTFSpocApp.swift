b1b36cfd451e0b48b602cb3eb4a49d78c2e8c3eb
bilicvitak <ivapapac22@gmail.com>
2024-11-10 12:11:15 +0100

    Initial commit

---
+++ GTFSpoc/GTFSpoc/GTFSpocApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTFSpocApp.swift
+//  GTFSpoc
+//
+//  Created by Iva on 10.11.2024..
+//
+
+import SwiftUI
+
+@main
+struct GTFSpocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
